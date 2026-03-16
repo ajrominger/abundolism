446c5e83c063abd4d258fde9a9328629b2f7fe8c
ajrominger <ajrominger@gmail.com>
2026-03-16 12:05:50 -1000

    finalized geom glm methods

--- R/geom_mle.R
+++ R/geom_mle.R
@@ -1,17 +1,25 @@
 #' @title Maximum likelihood estimation for geometric glm
 #'
 #' @description
-#' Find maximum likelihood parameters for glm assuming geometric
-#' distribution and logit link
+#'     Find maximum likelihood parameters for glm assuming geometric
+#'     distribution and logit link
 #'
 #' @param formula an object of class "formula" that describes the model
 #' @param data an optional data.frame where the data live
+#' @param ... arguments passed to `control` of `optim`
 #'
-#' @returns an object of class `glmGeom` inheriting from `glm` and `lm`
+#' @details
+#'     Some methods that work for objects of class `glm` and `lm` will
+#'     also work for the output of this function, specifically:
+#'     `print`, `coef`, `logLik`, and `AIC`. A custom likelihood ratio test
+#'     function is provided by `lrt_geom`
+#'
+#' @returns an object of class `glmGeom` inspired by, but not strictly
+#'     inheriting from, `glm` and `lm`
 #'
 #' @export
 
-geomglm <- function(formula, data) {
+geom_glm <- function(formula, data, ...) {
     if(missing(data)) {
         m <- model.frame(formula)
     } else {
@@ -27,9 +35,8 @@ geomglm <- function(formula, data) {
     p0 <- as.formula(m) |> lm() |> coef()
 
     # mle
-    fit <- optim(p0, fn = geo_ll, mx = X, my = y, method = "BFGS",
-                 hessian = TRUE,
-                 control = list(fnscale = -1))
+    fit <- optim(p0, fn = geo_ll, mx = X, my = y,
+                 control = list(fnscale = -1, ...))
 
     if(fit$convergence != 0) {
         stop("bad convergence when maximizing log likelihood function")
@@ -43,31 +50,23 @@ geomglm <- function(formula, data) {
     err <- y - fitted_vals
     ll <- structure(fit$value, df = ncol(X), class = "logLik")
 
-    # output needed for `glm` and `lm`
+    # subset of output from `glm` and `lm`
     res <- list(
         coefficients = beta_hat,
-        fitted.values = fitted_vals,
-        residuals = err,
-        linear.predictors = lin_pred,
-        df.residual = nrow(X) - ncol(X),
-        df.null = nrow(X) - 1,
-        rank = ncol(X),
+        # fitted.values = fitted_vals,
+        # residuals = err,
+        # linear.predictors = lin_pred,
+        # rank = ncol(X),
         call = match.call(),
-        terms = attr(m, "terms"),
-        model = m,
         x = X,
         y = y,
-        qr = qr(X),
-        family = list(family = "binomial", link = "logit", dispersion = 1),
         deviance = -2 * fit$value,
-        null.deviance = -2 * null_geo_ll(y),
-        converged = fit$convergence == 0,
         aic = 2 * (ncol(X) - fit$value),
         logLik = ll,
-        R = solve(fit$hessian)
+        terms = attr(m, "terms")
     )
 
-    class(res) <- c("glmGeom", "glm", "lm")
+    class(res) <- "glmGeom"
 
     return(res)
 
@@ -75,15 +74,110 @@ geomglm <- function(formula, data) {
 
 }
 
-boo <- geomglm(y ~ x + I(x^2))
-boo0 <- geomglm(y ~ x)
-anova(boo, boo0)
 
-summary(boo)
+#' @export
+logLik.glmGeom <- function(object) {
+    object$logLik
+}
 
-foo0 <- glm(y ~ 1, family = "poisson")
 
-anova(foo, foo0)
+#' @export
+print.glmGeom <- function(object) {
+    cat("Call:  ")
+    print(object$call)
+
+    cat("\nCoefficients:", "\n")
+    print(object$coefficients)
+
+    cat("\nLog likelihood:", "\n")
+    print(logLik(object))
+
+}
+
+
+#' @title Likelihood ratio test for geometric glm
+#'
+#' @description
+#'     Preform likelihood ratio test on two models fitted with `geom_glm`
+#'     distribution and logit link
+#'
+#' @param mod0 reduced model
+#' @param mod1 full model
+#'
+#'
+#' @returns an ANOVA table like object inheriting from `anova` and `data.frame`.
+#'     The LR test statistic can be extracted with `$Chisq[2]` and the P-value
+#'     extracted with \code{$`Pr(>Chisq)`}.
+#'
+#' @export
+
+lrt_geom <- function(mod0, mod1) {
+    ll0 <- logLik(mod0)
+    ll1 <- logLik(mod1)
+    df0 <- attr(ll0, "df")
+    df1 <- attr(ll1, "df")
+
+
+    lr <- -2 * (ll0 - ll1)
+    df <- df1 - df0
+
+    res <- data.frame(df = c(df0, df1),
+                      LogLik = c(ll0, ll1),
+                      Df = c(NA, df1 - df0),
+                      Chisq = c(NA, lr),
+                      Pr = c(NA, pchisq(lr, df, lower.tail = FALSE)))
+    names(res)[1] <- "#Df"
+    names(res)[5] <- "Pr(>Chisq)"
+
+
+    rownames(res) <- c("Model 0", "Model 1")
+
+    attr(res, "heading") <- c("Likelihood ratio test\n",
+                              sprintf("Model 0: %s\nModel 1: %s\n",
+                                      as.character(mod0$call)[2],
+                                      as.character(mod1$call)[2]))
+
+    class(res) <- c("anova", "data.frame")
+
+    res
+}
+
+
+#' @title Simple prediction for geometric glm's
+#'
+#' @description
+#'     Predict response or link function
+#'
+#' @param mod fitted `geomGLM` object
+#' @param newdata optional `data.frame` of new predictor variable values
+#' @param type whether to predict response or link function values
+#'
+#' @returns column vector of predictions
+#'
+#' @export
+
+pred_geom <- function(mod, newdata, type = c("response", "link")) {
+    if(missing(newdata)) {
+        mm <- model.matrix(mod)
+    } else {
+        tt <- delete.response(mod$terms)
+        m <- model.frame(tt, newdata)
+        mm <- model.matrix(tt, m)
+    }
+
+    lin <- mm %*% coef(mod)
+    pr <- 1 / (1 + exp(-lin))
+
+    # return either prob (=link) or response
+    type <- match.arg(type, c("response", "link"))
+
+    if(type == "response") {
+        return(1 / (1 - pr))
+    } else {
+        return(pr)
+    }
+}
+
 
 # non-exported function for calculating log likelihood
 #' @param pars vector of parameter values
@@ -107,30 +201,3 @@ geo_ll <- function(pars, mx, my) {
 }
 
 
-# non-exported function for calculating null log likelihood
-#' @param y response data
-
-null_geo_ll <- function(y) {
-    p <- 1 - 1 / mean(y)
-
-    sum(log(1 - p) + (y - 1) * log(p))
-}
-
-
-logLik.glmGeom <- function(object) {
-    object$logLik
-}
-
-geometric <- function() {
-    structure(
-        list(
-            family = "geometric",
-            link = "logit",
-            linkfun = qlogis,
-            linkinv = plogis,
-            variance = function(mu) mu + mu^2,
-            dispersion = 1
-        ),
-        class = "family"
-    )
-}
